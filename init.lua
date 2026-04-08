7e2fdc22eb3df61c6f2e438f2b6b1644b7ed3364
absolutedev <motpetrov080712@gmail.com>
2026-04-08 08:21:02 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -265,7 +265,7 @@ if readFile and writeFile then
                 local content
 
                 if not ran then
-                    content = game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/Hydroxide/" .. branch .. '/' .. asset .. ".lua")
+                    content = game:HttpGetAsync("https://raw.githubusercontent.com/" .. user .. "/hydroxide-mod/" .. branch .. '/' .. asset .. ".lua")
                     writeFile(file, content)
                 else
                     content = result
